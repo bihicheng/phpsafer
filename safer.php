e5e8b15ab2d9560e0b637a8ab90dc668533b1e65
bihicheng <bihicheng@gmail.com>
2011-12-23 00:26:57 +0800

    first commit

---
+++ safer.php
@@ -0,0 +1,127 @@
+<?php
+require_once 'error.php';
+
+class Html implements Str
+{
+    private $str, $type;
+
+    function __construct($html)
+    {
+        $this->setType(0x00);
+        $this->setStr($html);
+    }
+
+    function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    function getType()
+    {
+        return $this->type;
+    }
+    function setStr($str)
+    {
+        $this->str = $str;
+    }
+    function getStr()
+    {
+        return $this->str;
+    }
+}
+
+class Sql implements Str
+{
+    private $str,$type;
+
+    function __construct($sql){
+        $this->setType(0x01);
+        $this->setStr($sql);
+    }
+
+    function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    function getType()
+    {
+        return $this->type;
+    }
+    function setStr($str)
+    {
+        $this->str = $str;
+    }
+    function getStr()
+    {
+        return $this->str;
+    }
+
+}
+
+
+interface Str
+{
+    function getType();
+    function getStr();
+}
+
+interface Safer_interface
+{
+    function safe_str();
+}
+
+abstract class Safer_abstract implements Safer_interface
+{
+    abstract function safe_str();
+}
+
+class Safer extends Safer_abstract
+{
+    public $str,$type;
+
+    function __construct(Str $s)
+    {
+        $this->str = $s->getStr();
+        $this->type = $s->getType();
+    }
+
+    function safe_str()
+    {
+        if ($this->type == 0x01) {
+            return $this->sql_safe($this->str);
+        }
+        else if($this->type == 0x00){
+            return $this->html_safe($this->str);
+        }
+    }
+
+    function sql_safe($str)
+    {
+        try{
+            if($sql = "'" . mysql_real_escape_string($str) . "'")
+                return $sql;
+            else
+                throw new Exception("$sql formart error");
+        }
+        catch(Exception $e)
+        {
+            trigger_error($e->getMessage());
+        }
+    }
+
+    function html_safe($str)
+    {
+        return  htmlentities($str, ENT_QUOTES, 'UTF-8');
+    }
+}
+
+//Usage:
+$a = "<a href=''/> <script>alert('Hello');</script>";
+$query_str = "test' AND 1=1;";
+//$str = "SELECT * FROM test WHERE name=$safe\G";
+
+$sql = new Sql($query_str);
+$html = new Html($a);
+$safer = new Safer($sql);
+var_dump($safer->safe_str());
